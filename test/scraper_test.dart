eb158bec54e4f42844568e8fb1bb5445a67abf11
mfikryrizal <secure.fikry@gmail.com>
2021-06-14 16:28:34 +0800

    added more unit tests and cleaned up unneededs

--- test/scraper_test.dart
+++ test/scraper_test.dart
@@ -1,6 +1,11 @@
+import 'dart:ui';
+import 'package:file/memory.dart';
+import 'package:http/http.dart' as http;
+
 import 'package:flutter_test/flutter_test.dart';
 import 'package:manga_flutter/data/manga_tile_data.dart';
 import 'package:manga_flutter/scraper/home_scraper.dart';
+import 'package:mime/mime.dart';
 
 void main() async {
   final HomeScraper homeScraper = HomeScraper();
@@ -22,5 +27,24 @@ void main() async {
         result.any((element) =>
             element.imgSrc.isEmpty || element.imgSrc.endsWith('null')),
         false);
+    expect(
+        result.any(
+            (element) => element.ref.isEmpty || element.ref.endsWith('null')),
+        false);
+  });
+
+  test('Get image for MangaTile', () async {
+    await homeScraper.loadPage();
+    final results = homeScraper.getMangaTiles();
+    var fs = MemoryFileSystem();
+    int count = 0;
+    for (MangaTileData m in results) {
+      var response = await http.get(Uri.parse(m.imgSrc));
+      var tf = await fs.file('temp-${count++}.jpg').create();
+      await tf.writeAsBytes(response.bodyBytes, flush: true);
+      var mime = lookupMimeType(tf.path, headerBytes: [0xFF, 0xD8]);
+      expect(mime?.startsWith('image'), true);
+      tf.delete();
+    }
   });
 }
